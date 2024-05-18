17ed818178006bd005ccd980c022926af273e49f
Bassma <bm105257@gmail.com>
2024-05-18 18:10:41 +0300

    up

--- src/components/Work.js
+++ src/components/Work.js
@@ -16,13 +16,13 @@ const Work = () => {
     <section className="section mt-6" id="work">
       <div className="container mx-auto">
         <h2 className="h2 leading-tight text-accent">My Latest Work.</h2>
-        <div className="flex flex-col lg:flex-row gap-x-10">
+        <div className="flex flex-col lg:flex-col gap-x-10">
           <motion.div
             variants={fadeIn("right", 0.2)}
             initial="hidden"
             whileInView={"show"}
             viewport={{ once: false, amount: 0.3 }}
-            className="flex-1 flex flex-row gap-y-12 mb-10 lg:mb-0"
+            className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0"
           >
             <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
               <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
